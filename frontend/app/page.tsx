bdd5966aada779da523a4255ca5918d65e341b93
Chunfeng Liu <cliu3@rocketsoftware.com>
2024-08-02 14:26:39 +0800

    feat: Add form

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,85 @@
 "use client";
 
-import { useState, useEffect, useRef } from "react";
+import { on } from "events";
+import {
+  useState,
+  useEffect,
+  useRef,
+  ChangeEventHandler,
+  ChangeEvent,
+} from "react";
 import { Web3 } from "web3";
+
+interface Address {
+  id: string;
+  label: string;
+  address: string;
+  error: string;
+  split: number;
+}
 
 export default function Home() {
   const [hasWalletWarning, setHasWalletWarning] = useState(false);
   const [currnetAccount, setCurrnetAccount] = useState(null);
   const web3 = useRef<Web3 | null>(null);
+
+  const [partners, setPartners] = useState([
+    {
+      id: "1",
+      label: "Parnter A",
+      address: "",
+      error: "",
+      split: 1,
+    },
+    {
+      id: "2",
+      label: "Parnter B",
+      address: "",
+      error: "",
+      split: 1,
+    },
+  ]);
+
+  const addressInputs = partners.map((partner, index) => {
+    return (
+      <PartnerInput
+        address={{
+          label: partner.label,
+          value: partner.address,
+          onChange: (value) => {
+            setPartners((oldPartnersState) => {
+              const newPartnersState = [...oldPartnersState];
+              newPartnersState[index].address = value;
+              return newPartnersState;
+            });
+          },
+          onBlur: (value) => {
+            setPartners((oldPartnersState) => {
+              const isValueAddress = web3.current?.utils.isAddress(value);
+              const newPartnersState = [...oldPartnersState];
+              newPartnersState[index].error = isValueAddress
+                ? ""
+                : "Invalid Address";
+              return newPartnersState;
+            });
+          },
+          error: partner.error,
+        }}
+        split={{
+          name: partner.label,
+          value: partner.split,
+          onChange: (value) => {
+            setPartners((oldPartnersState) => {
+              const newPartnersState = [...oldPartnersState];
+              newPartnersState[index].split = value;
+              return newPartnersState;
+            });
+          },
+        }}
+        key={partner.label}
+      />
+    );
+  });
 
   const checkIfWalletIsConnected = () => {
     return Boolean((window as any).ethereum);
@@ -80,6 +153,105 @@ export default function Home() {
     );
   }
 
+  function AddressInput({
+    label,
+    value,
+    onChange,
+    onBlur,
+    error,
+  }: {
+    label: string;
+    value: string;
+    onChange: (value: string) => void;
+    onBlur: (value: string) => void;
+    error: string;
+  }) {
+    return (
+      <div className="flex flex-col">
+        <label htmlFor={label}>{label}</label>
+        <input
+          name={label}
+          value={value}
+          type="text"
+          onChange={(event) => onChange(event.target.value)}
+          onBlur={(event) => onBlur(event.target.value)}
+          className={"form-input rounded px-4 py-3"}
+          placeholder="Wallet Address"
+        />
+        {error ? (
+          <p className="text-red-600 text-sm">{error}</p>
+        ) : (
+          <p className="text-sm">&nbsp;</p>
+        )}
+      </div>
+    );
+  }
+
+  function SplitInput({
+    name,
+    value,
+    onChange,
+  }: {
+    name: string;
+    value: number;
+    onChange: (value: number) => void;
+  }) {
+    return (
+      <div className="flex flex-col">
+        <label htmlFor={`split-${name}`}>Split</label>
+        <input
+          name={`split-${name}`}
+          value={value}
+          type="number"
+          onChange={(event) => onChange(Number(event.target.value))}
+          className={"form=-input rounded px-4 py-3"}
+          placeholder={1 + ""}
+          min={1}
+          step={1}
+        />
+      </div>
+    );
+  }
+
+  function PartnerInput({
+    address,
+    split,
+  }: {
+    address: {
+      label: string;
+      value: string;
+      onChange: (value: string) => void;
+      onBlur: (value: string) => void;
+      error: string;
+    };
+    split: {
+      name: string;
+      value: number;
+      onChange: (value: number) => void;
+    };
+  }) {
+    return (
+      <div className={"flex"}>
+        <div className={"mr-3 w-full"}>
+          <AddressInput
+            label={address.label}
+            value={address.value}
+            onChange={address.onChange}
+            onBlur={address.onBlur}
+            error={address.error}
+          />
+        </div>
+        <div className={"w-1/5"}>
+          <SplitInput
+            name={split.name}
+            value={split.value}
+            onChange={split.onChange}
+          />
+        </div>
+      </div>
+    );
+  }
+
   useEffect(() => {
     if (web3.current) {
       return;
@@ -106,12 +278,10 @@ export default function Home() {
             distributes the payments to the partnership contract in a
             predetermined split ratio.
           </p>
-          {hasWalletWarning ? (
+          {hasWalletWarning && (
             <p className="mt-4 text-red-600">
               You will need MetaMask or equivalent to use this app.
             </p>
-          ) : (
-            <p>Your wallet is connected!</p>
           )}
           {!currnetAccount && (
             <div className="mt-4">
@@ -123,6 +293,12 @@ export default function Home() {
             </div>
           )}
         </section>
+
+        { currnetAccount && (
+          <div className="grid grid-cols-1 gap-3 mb-6">
+            {addressInputs}
+          </div>
+        )}
       </main>
     </div>
   );
